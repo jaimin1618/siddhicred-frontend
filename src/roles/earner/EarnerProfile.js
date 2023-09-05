9a7e2a00f6b7d147c4fcc412d835f9c79f06073b
jaimin1618 <jaimin.chokhawala@gmail.com>
2023-09-05 20:01:33 +0530

    contract JSON file changed

--- src/roles/earner/EarnerProfile.js
+++ src/roles/earner/EarnerProfile.js
@@ -64,13 +64,7 @@ const EarnerProfile = () => {
               // fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
-            >
-              {/* <path
-                fillRule="evenodd"
-                d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                clipRule="evenodd"
-              ></path> */}
-            </svg>
+            ></svg>
           </div>
         </div>
         <div className="lg:w-2/3 flex justify-start">
